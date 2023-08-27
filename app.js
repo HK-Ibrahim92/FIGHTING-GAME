2896400686cb599a1d97cb479acd31887fefd78f
Syed Ibrahim <Syedzada786alam@gmail.com>
2023-08-28 01:17:56 +0500

    Thumbnail UI, character file

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 var imagesrc = ["https://media2.giphy.com/media/7XqRXKYV9odtm/giphy.gif?cid=ecf05e476l5ua9v3lgudn3aup4dgr01e2lrl6s49mo2dw6xe&ep=v1_gifs_search&rid=giphy.gif&ct=g", "http://twistedsifter.com/wp-content/uploads/2013/05/animated-gifs-of-fighting-game-backgrounds-36.gif", "http://twistedsifter.com/wp-content/uploads/2013/05/animated-gifs-of-fighting-game-backgrounds-47.gif", "http://twistedsifter.com/wp-content/uploads/2013/05/animated-gifs-of-fighting-game-backgrounds-48.gif", "http://twistedsifter.com/wp-content/uploads/2013/05/animated-gifs-of-fighting-game-backgrounds-12.gif", "http://twistedsifter.com/wp-content/uploads/2013/05/animated-gifs-of-fighting-game-backgrounds-18.gif"]
 
 var count = 0;
-
+let userName;
 var body = document.getElementById("body")
+
+
+
+// Wait for the DOM to be fully loaded
+ function username() {
+    // Use SweetAlert to prompt for user's name
+    Swal.fire({
+        title: 'Enter Your Name',
+        input: 'text',
+        inputLabel: 'Name',
+        inputPlaceholder: 'Enter your name...',
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        cancelButtonText: 'Cancel',
+        allowOutsideClick: false,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Name cannot be empty!';
+            }
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            
+             userName = result.value;
+            // Now you can use the userName in your game
+            localStorage.setItem('userName', userName);
+            gotologin()
+        }
+    });
+};
+
 
 
 //----------- PROGRESS BAR------------
@@ -41,6 +72,12 @@ function backbg() {
 
 function gotologin() {
     window.location.href = "main.html"
+    swal({
+        title: "Hello!"+storedUserName+"!",
+        
+        timer: 2000
+      });
+      
 }
 function gamestart() {
     var playerInfo = document.querySelector('.playerinfo');
